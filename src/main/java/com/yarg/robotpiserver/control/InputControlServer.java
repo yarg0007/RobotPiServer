4e3fe5044e6c98d33b997d3a815f2c3fafe666cc
Ben Yarger <byarger@asbtechnologies.com>
2022-04-30 17:05:35 -0700

    Add PWMController unit tests and calculate code coverage
    - Adding unit tests to PWMController
    - Adding Jacoco code coverage to the pom
    - Adding TestNG suite config to project

--- src/main/java/com/yarg/robotpiserver/control/InputControlServer.java
+++ src/main/java/com/yarg/robotpiserver/control/InputControlServer.java
@@ -29,116 +29,117 @@ import com.yarg.robotpiserver.audio.AudioLevelListener;
 public class InputControlServer extends Thread implements AudioLevelListener{
 
 	private static final int PORT = 49801;
-	
+
 	private boolean running = false;
-	
+
 	// Should be a value between 0 and 100.
 	private int talkSoundLevel = 0;
-	
+
 	// This is a copy of the last sound level received from audio input. Can
 	// be as large as needed from the audio source.
 	private int previousSoundLevel = 0;
-	
+
 	PWMController pwmController;
-	
+
 	/** The connected client. Setup to only allow a single client connection.*/
 	private DatagramSocket serverDatagramSocket = null;
-	
+
 	public InputControlServer() {
 		initialize();
 	}
-	
+
 	public void startInputControlServer() {
 		pwmController.startPWMController();
 		running = true;
 		this.start();
 		System.out.println("Input control server ready.");
 	}
-	
+
 	public void stopInputControlServer() {
-		
+
 		running = false;
 		this.interrupt();
-		
+
 		serverDatagramSocket.close();
 		serverDatagramSocket = null;
-		
+
 		pwmController.stopPWMController();
-		
+
 		System.out.println("Input control server stopped.");
 	}
-	
+
 	/* (non-Javadoc)
 	 * @see java.lang.Thread#run()
 	 */
-	public void run() {			
+	@Override
+	public void run() {
 
-			byte[] datagramBuffer = new byte[64];
-			DatagramPacket datagramPacket = new DatagramPacket(datagramBuffer, datagramBuffer.length);
-			
-//			int numTokens = 6;
-			
-			int driveInput = 0;
-			int turnInput = 0;
-			int headLiftInput = 0;
-			int headTurnInput = 0;
-			int talking = 0;
-			int openMouth = 0;
-			
-			byte[] rawData;
-			String[] tokens;
-			
-			try {
+		byte[] datagramBuffer = new byte[64];
+		DatagramPacket datagramPacket = new DatagramPacket(datagramBuffer, datagramBuffer.length);
 
-				while (running) {
-					
-					serverDatagramSocket.receive(datagramPacket);
-					
-					rawData = datagramPacket.getData();
-					String data = new String(rawData);
-					data = data.substring(0, data.indexOf(":"));
+		//			int numTokens = 6;
 
-					tokens = data.split(",");
-					
-					driveInput = 0;
-					turnInput = 0;
-					headLiftInput = 0;
-					headTurnInput = 0;
-					talking = 0;
-					openMouth = 0;
-					
-					driveInput = Integer.valueOf(tokens[0]);
-					turnInput = Integer.valueOf(tokens[1]);
-					headLiftInput = Integer.valueOf(tokens[2]);
-					headTurnInput = Integer.valueOf(tokens[3]);
-					talking = Integer.valueOf(tokens[4]);
-					openMouth = Integer.valueOf(tokens[5]);
-					
-					pwmController.setDriveInput(driveInput);
-					pwmController.setTurnInput(turnInput);
-					pwmController.setHeadLiftInput(headLiftInput);
-					pwmController.setHeadTurnInput(headTurnInput);
-					pwmController.setTalkInput(talking, talkSoundLevel, openMouth);
-						
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			} catch (NumberFormatException nfe) {
-				//ignore.
+		int driveInput = 0;
+		int turnInput = 0;
+		int headLiftInput = 0;
+		int headTurnInput = 0;
+		int talking = 0;
+		int openMouth = 0;
+
+		byte[] rawData;
+		String[] tokens;
+
+		try {
+
+			while (running) {
+
+				serverDatagramSocket.receive(datagramPacket);
+
+				rawData = datagramPacket.getData();
+				String data = new String(rawData);
+				data = data.substring(0, data.indexOf(":"));
+
+				tokens = data.split(",");
+
+				driveInput = 0;
+				turnInput = 0;
+				headLiftInput = 0;
+				headTurnInput = 0;
+				talking = 0;
+				openMouth = 0;
+
+				driveInput = Integer.valueOf(tokens[0]);
+				turnInput = Integer.valueOf(tokens[1]);
+				headLiftInput = Integer.valueOf(tokens[2]);
+				headTurnInput = Integer.valueOf(tokens[3]);
+				talking = Integer.valueOf(tokens[4]);
+				openMouth = Integer.valueOf(tokens[5]);
+
+				pwmController.setDriveInput(driveInput);
+				pwmController.setTurnInput(turnInput);
+				pwmController.setHeadLiftInput(headLiftInput);
+				pwmController.setHeadTurnInput(headTurnInput);
+				pwmController.setTalkInput(talking, talkSoundLevel, openMouth);
+
 			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (NumberFormatException nfe) {
+			//ignore.
+		}
 
 	}
-	
+
 	// -------------------------------------------------------------------------
 	// Private methods
 	// -------------------------------------------------------------------------
-	
+
 	private void initialize() {
-		
+
 		if (running) {
 			running = false;
 			this.interrupt();
-			
+
 			// Let the thread terminate and then proceed.
 			try {
 				Thread.sleep(2000);
@@ -146,27 +147,27 @@ public class InputControlServer extends Thread implements AudioLevelListener{
 				ie.printStackTrace();
 			}
 		}
-		
+
 		if (serverDatagramSocket != null) {
 			serverDatagramSocket.close();
 		}
-		
+
 		try {
 			serverDatagramSocket = new DatagramSocket(PORT);
 		} catch (SocketException e) {
 			e.printStackTrace();
 		}
-		
-		pwmController = PWMController.getInstance();
+
+		pwmController = new PWMController();
 	}
-	
+
 	// -------------------------------------------------------------------------
 	//	Methods required by AudioLevelListener
 	// -------------------------------------------------------------------------
 
 	@Override
 	public void audioLevelUpdate(int audioLevelBaseline, int currentAudioLevel) {
-		
+
 		if (currentAudioLevel <= audioLevelBaseline) {
 			talkSoundLevel = 0;
 		} else if (currentAudioLevel > previousSoundLevel) {
@@ -174,7 +175,7 @@ public class InputControlServer extends Thread implements AudioLevelListener{
 		} else {
 			talkSoundLevel = 0; //(int) (((float)currentAudioLevel - (float)audioLevelBaseline)/((float)previousSoundLevel - (float)audioLevelBaseline) * 100.0f);
 		}
-		
+
 		if (talkSoundLevel > 100) {
 			talkSoundLevel = 100;
 		}
--- src/main/java/com/yarg/robotpiserver/control/PWMController.java
+++ src/main/java/com/yarg/robotpiserver/control/PWMController.java
@@ -25,26 +25,28 @@ import com.pi4j.io.i2c.I2CBus;
 import com.pi4j.io.i2c.I2CDevice;
 import com.pi4j.io.i2c.I2CFactory;
 
-public class PWMController extends Thread {
+public final class PWMController extends Thread {
 
 	public final static int HERTZ = 50;
-	
-	public final static int SUBADR1       = 0x02;
-	public final static int SUBADR2       = 0x03;
-	public final static int SUBADR3       = 0x04;
-	public final static int MODE1         = 0x00;
-	public final static int PRESCALE      = 0xFE;
-	public final static int LED0_ON_L     = 0x06;
-	public final static int LED0_ON_H     = 0x07;
-	public final static int LED0_OFF_L    = 0x08;
-	public final static int LED0_OFF_H    = 0x09;
-	public final static int ALL_LED_ON_L  = 0xFA;
-	public final static int ALL_LED_ON_H  = 0xFB;
+
+	public final static int SUBADR1 = 0x02;
+	public final static int SUBADR2 = 0x03;
+	public final static int SUBADR3 = 0x04;
+	public final static int MODE1 = 0x00;
+	public final static int PRESCALE = 0xFE;
+	public final static int LED0_ON_L = 0x06;
+	public final static int LED0_ON_H = 0x07;
+	public final static int LED0_OFF_L = 0x08;
+	public final static int LED0_OFF_H = 0x09;
+	public final static int ALL_LED_ON_L = 0xFA;
+	public final static int ALL_LED_ON_H = 0xFB;
 	public final static int ALL_LED_OFF_L = 0xFC;
 	public final static int ALL_LED_OFF_H = 0xFD;
-	
+
+	// 0x40 obtained through sudo i2cdetect -y 1
+	private static final int INIT_ADDRESS = 0x40;
 	private static boolean verbose = true;
-	
+
 	// Output Channels
 	private static final int DRIVE_CHANNEL = 15;
 	private static final int DRIVE_DIRECTION_CHANNEL = 14;
@@ -52,93 +54,81 @@ public class PWMController extends Thread {
 	private static final int HEAD_LIFT_CHANNEL = 12;
 	private static final int HEAD_TURN_CHANNEL = 11;
 	private static final int TALK_CHANNEL = 10;
-	
+
 	// Drive channel max and min
-//	private static final int DRIVE_CHANNEL_PWM_MAX = 4095;
+	// private static final int DRIVE_CHANNEL_PWM_MAX = 4095;
 	private static final int DRIVE_CHANNEL_PWM_MIN = 0;
-	
+
 	// Drive direction channel max and min
 	private static final int DRIVE_DIRECTION_CHANNEL_PWM_MAX = 4095;
 	private static final int DRIVE_DIRECTION_CHANNEL_PWM_MIN = 0;
-		  
+
 	// Turn channel max, min and center
-//	private static final int TURN_CHANNEL_PWM_CENTER = 325;
+	// private static final int TURN_CHANNEL_PWM_CENTER = 325;
 	private static final int TURN_CHANNEL_PWM_MAX = 400;
 	private static final int TURN_CHANNEL_PWM_MIN = 250;
-	
+
 	// Head lift channel max, min and center
-//	private static final int HEAD_LIFT_CHANNEL_PWM_CENTER = 200;
+	// private static final int HEAD_LIFT_CHANNEL_PWM_CENTER = 200;
 	private static final int HEAD_LIFT_CHANNEL_PWM_MAX = 350;
 	private static final int HEAD_LIFT_CHANNEL_PWM_MIN = 200;
-	
+
 	// Head turn channel max, min and center
-//	private static final int HEAD_TURN_CHANNEL_PWM_CENTER = 325;
+	// private static final int HEAD_TURN_CHANNEL_PWM_CENTER = 325;
 	private static final int HEAD_TURN_CHANNEL_PWM_MAX = 400;
 	private static final int HEAD_TURN_CHANNEL_PWM_MIN = 250;
-	
+
 	// Talk channel max, min and center
-//	private static final int TALK_CHANNEL_PWM_CENTER = 295;
+	// private static final int TALK_CHANNEL_PWM_CENTER = 295;
 	private static final int TALK_CHANNEL_PWM_MAX = 250;
 	private static final int TALK_CHANNEL_PWM_MIN = 340;
-	
-	private I2CBus    bus;
+
+	private I2CBus bus;
 	private I2CDevice servoDriver;
-	
+
 	// Used for calculating change in acceleration.
 	private int previousDriveInput;
-	
+
 	// Used for timing slow down.
 	private long lastInputTime;
 	private boolean running;
-	
-	private static PWMController pwmController;
-	
+
 	/**
-	 * Get the PWMController singleton.
-	 * @return PWMController singleton instance.
+	 * Use this to initialize the servoDriver and bus according to the default
+	 * address.
 	 */
-	public static PWMController getInstance()
-	{
-		if (pwmController == null) {
-			pwmController = new PWMController();
-		}
-		return pwmController;
+	public PWMController() {
+		init();
 	}
-	
+
 	/**
-	 * Create the singleton PWM controller instance.
+	 * Use this to initialzie with a pre-initialized bus and servoDriver.
+	 *
+	 * @param bus
+	 *            Bus to use.
+	 * @param servoDriver
+	 *            Servo driver to use.
 	 */
-	private PWMController()
-	{
-		previousDriveInput = 0;
-		init(0x40); // 0x40 obtained through sudo i2cdetect -y 1
-		lastInputTime = System.currentTimeMillis();
-		running = false;
+	public PWMController(I2CBus bus, I2CDevice servoDriver) {
+		this.bus = bus;
+		this.servoDriver = servoDriver;
 	}
-	
+
 	/**
 	 * Start the PWM controller loop.
 	 */
 	public void startPWMController() {
-		running= true;
+		running = true;
 		this.start();
 	}
-	
+
 	/**
-	 * Stop the PWM controller loop.
+	 * Stop the PWM controller loop. Bring all motors to a sane stop.
 	 */
 	public void stopPWMController() {
-		
-		running = false;
-		this.interrupt();
-		
-		try {
-			sleep(1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+
+		quickStop();
+
 		// Bring robot to stop.
 		setPWM(DRIVE_CHANNEL, 0, 0);
 		setPWM(TURN_CHANNEL, 0, 0);
@@ -146,42 +136,58 @@ public class PWMController extends Thread {
 		setPWM(HEAD_TURN_CHANNEL, 0, 0);
 		setPWM(TALK_CHANNEL, 0, 0);
 	}
-	
+
+	/**
+	 * Stop the thread. No shutdown operations of the servos is managed by this
+	 * method.
+	 */
+	protected void quickStop() {
+		running = false;
+		this.interrupt();
+
+		running = false;
+		this.interrupt();
+
+		try {
+			sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
 	/**
 	 * Set the drive input.
-	 * @param input Input values are expected in the range -100 through +100.
+	 *
+	 * @param input
+	 *            Input values are expected in the range -100 through +100.
 	 */
 	public void setDriveInput(int input) {
 
-		if (!running) {
-			return;
-		}
-		
 		// Lock to our input range.
 		if (input < -100) {
 			input = -100;
 		} else if (input > 100) {
 			input = 100;
 		}
-		
+
 		// Timestamp the current input. If over a certain amount of time
 		// the drive will automatically slow down.
 		lastInputTime = System.currentTimeMillis();
-	
+
 		if (input == 0) {
-			previousDriveInput = previousDriveInput/2;
+			previousDriveInput = previousDriveInput / 2;
 		} else if (input - previousDriveInput > 0) {
 			previousDriveInput++;
 		} else if (input - previousDriveInput < 0) {
 			previousDriveInput--;
 		}
-		
+
 		if (previousDriveInput > 100) {
 			previousDriveInput = 100;
 		} else if (previousDriveInput < -100) {
 			previousDriveInput = -100;
 		}
-		
+
 		if (previousDriveInput < 0) {
 			// set to reverse
 			setPWM(DRIVE_DIRECTION_CHANNEL, 0, DRIVE_DIRECTION_CHANNEL_PWM_MIN);
@@ -189,20 +195,20 @@ public class PWMController extends Thread {
 			// set to forward
 			setPWM(DRIVE_DIRECTION_CHANNEL, 0, DRIVE_DIRECTION_CHANNEL_PWM_MAX);
 		}
-		
+
 		// 1) Calculate the percent of total speed as drivePercentValue.
 		// 2) Calculate the percentage of MAX speed to send out as pwm.
 		// 3) Make sure pwm value is positive.
-		float drivePercentValue = (float)previousDriveInput / 100.0f;
-		int pwmValue = (int)(drivePercentValue*(float)DRIVE_DIRECTION_CHANNEL_PWM_MAX);
+		float drivePercentValue = previousDriveInput / 100.0f;
+		int pwmValue = (int) (drivePercentValue * DRIVE_DIRECTION_CHANNEL_PWM_MAX);
 		pwmValue = Math.abs(pwmValue);
-		
+
 		// If PWM is < 25 don't move This is our change direction buffer.
 		if (pwmValue < 5) {
 			setPWM(DRIVE_CHANNEL, 0, DRIVE_CHANNEL_PWM_MIN);
 		} else {
 			setPWM(DRIVE_CHANNEL, 0, pwmValue);
-//			System.out.println("Set drive input: "+pwmValue);
+			// System.out.println("Set drive input: "+pwmValue);
 		}
 	}
 
@@ -220,27 +226,30 @@ public class PWMController extends Thread {
 		int pwmValue = calculateServoPWMValue(input, HEAD_TURN_CHANNEL_PWM_MIN, HEAD_TURN_CHANNEL_PWM_MAX);
 		setPWM(HEAD_TURN_CHANNEL, 0, pwmValue);
 	}
-	
+
 	public void setTalkInput(int isTalking, int talkingLevel, int openMouth) {
-		
+
 		if (openMouth == 1) {
 			setPWM(TALK_CHANNEL, 0, TALK_CHANNEL_PWM_MAX);
 		} else if (isTalking == 1) {
-			
-			int pwmValue = TALK_CHANNEL_PWM_MIN;
-			
-			if (talkingLevel == 100) {
-				pwmValue = TALK_CHANNEL_PWM_MAX;
+
+			if (talkingLevel <= 0) {
+				talkingLevel = 0;
+			} else if (talkingLevel >= 100) {
+				talkingLevel = 100;
 			}
-			
-//			int pwmValue = calculateServoPWMValue(talkingLevel, TALK_CHANNEL_PWM_MIN, TALK_CHANNEL_PWM_MAX);
+
+			int pwmValue = (int) (TALK_CHANNEL_PWM_MIN
+					- (TALK_CHANNEL_PWM_MIN - TALK_CHANNEL_PWM_MAX) * (talkingLevel / 100.0f));
+
 			setPWM(TALK_CHANNEL, 0, pwmValue);
 		} else {
 			setPWM(TALK_CHANNEL, 0, TALK_CHANNEL_PWM_MIN);
 		}
 	}
-	  
-	// Thread to slow vehicle down if no input occurred in last 40ms (25hz update)
+
+	// Thread to slow vehicle down if no input occurred in last 40ms (25hz
+	// update)
 	@Override
 	public void run() {
 
@@ -263,66 +272,56 @@ public class PWMController extends Thread {
 	}
 
 	// -----------------------------------------------------------------------
-	// Private Methods
+	// Protected Methods
 	// -----------------------------------------------------------------------
 
+	protected void setPWM(int channel, int on, int off) {
+		try {
+			servoDriver.write(LED0_ON_L + 4 * channel, (byte) (on & 0xFF));
+			servoDriver.write(LED0_ON_H + 4 * channel, (byte) (on >> 8));
+			servoDriver.write(LED0_OFF_L + 4 * channel, (byte) (off & 0xFF));
+			servoDriver.write(LED0_OFF_H + 4 * channel, (byte) (off >> 8));
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
+
 	/**
-	 * Calculate the servo pwm value based on input and MIN/MAX servo PWM values.
-	 * This is used to calculate the rotation of a servo from an open (MAX) to
-	 * closed (MIN) position.
-	 * 
-	 * @param input Input in the range of -100 to +100.
-	 * @param MIN Minimum PWM value allowed.
-	 * @param MAX Maximum PWM value allowed.
+	 * Calculate the servo pwm value based on input and MIN/MAX servo PWM
+	 * values. This is used to calculate the rotation of a servo from an open
+	 * (MAX) to closed (MIN) position.
+	 *
+	 * @param input
+	 *            Input in the range of -100 to +100.
+	 * @param MIN
+	 *            Minimum PWM value allowed.
+	 * @param MAX
+	 *            Maximum PWM value allowed.
 	 * @return PWM value corresponding to input.
 	 */
-	private int calculateServoPWMValue(int input, float MIN, float MAX) {
-		
+	protected int calculateServoPWMValue(int input, float MIN, float MAX) {
+
 		if (input < -100) {
 			input = -100;
 		} else if (input > 100) {
 			input = 100;
 		}
-		
+
 		// Linear curve
-		float slope = ((float)MAX - (float)MIN)/200.0f;
-		float result = ((float)input + 100.0f) * slope + MIN;
-		return (int)result;
+		float slope = (MAX - MIN) / 200.0f;
+		float result = (input + 100.0f) * slope + MIN;
+		return (int) result;
 	}
 
-	private void init(int address) {
-
-		try
-	    {
-			// Get I2C bus
-			bus = I2CFactory.getInstance(I2CBus.BUS_1); // Depends onthe RasPI version
-			
-			if (verbose) {
-				System.out.println("Connected to bus. OK.");
-			}
-
-			// Get the device itself
-			servoDriver = bus.getDevice(address); 
-
-			if (verbose) {
-				System.out.println("Connected to device. OK.");
-			}
-
-			// Reseting
-			servoDriver.write(MODE1, (byte)0x00);
-	    }
-	    catch (IOException e)
-	    {
-	      System.err.println(e.getMessage());
-	    }
-
-		setPWMFreq(HERTZ); // Set frequency to 60 Hz    
-	}
-
-	private void setPWMFreq(int freq)
-	{
+	/**
+	 * Set the PWM signaling frequency.
+	 *
+	 * @param freq
+	 *            Frequency to set.
+	 */
+	protected void setPWMFreq(int freq) {
 		float prescaleval = 25000000.0f; // 25MHz
-		prescaleval /= 4096.0;           // 12-bit
+		prescaleval /= 4096.0; // 12-bit
 		prescaleval /= freq;
 		prescaleval -= 1.0;
 		if (verbose) {
@@ -337,53 +336,53 @@ public class PWMController extends Thread {
 		}
 
 		try {
-			byte oldmode = (byte)servoDriver.read(MODE1);
-			byte newmode = (byte)((oldmode & 0x7F) | 0x10); // sleep
-			servoDriver.write(MODE1, newmode);              // go to sleep
-			servoDriver.write(PRESCALE, (byte)(Math.floor(prescale)));
+			byte oldmode = (byte) servoDriver.read(MODE1);
+			byte newmode = (byte) ((oldmode & 0x7F) | 0x10); // sleep
+			servoDriver.write(MODE1, newmode); // go to sleep
+			servoDriver.write(PRESCALE, (byte) (Math.floor(prescale)));
 			servoDriver.write(MODE1, oldmode);
-			
+
 			Thread.sleep(5);
 
-			servoDriver.write(MODE1, (byte)(oldmode | 0x80));
-		}
-		catch (IOException ioe) {
+			servoDriver.write(MODE1, (byte) (oldmode | 0x80));
+		} catch (IOException ioe) {
 			ioe.printStackTrace();
-		}
-		catch (InterruptedException ie) {
+		} catch (InterruptedException ie) {
 			ie.printStackTrace();
 		}
 	}
 
-	private void setPWM(int channel, int on, int off) {
-		try
-		{
-			servoDriver.write(LED0_ON_L  + 4 * channel, (byte)(on & 0xFF));
-			servoDriver.write(LED0_ON_H  + 4 * channel, (byte)(on >> 8));
-			servoDriver.write(LED0_OFF_L + 4 * channel, (byte)(off & 0xFF));
-			servoDriver.write(LED0_OFF_H + 4 * channel, (byte)(off >> 8));
-		}
-		catch (IOException ioe)
-		{
-			ioe.printStackTrace();
+	protected void init() {
+
+		previousDriveInput = 0;
+		lastInputTime = System.currentTimeMillis();
+		running = false;
+
+		try {
+			// Get I2C bus unless it is already provided.
+			// Depends on the RasPI version.
+			if (bus == null) {
+				bus = I2CFactory.getInstance(I2CBus.BUS_1);
+			}
+
+			if (verbose) {
+				System.out.println("Connected to bus. OK.");
+			}
+
+			// Get the device itself.
+			servoDriver = bus.getDevice(INIT_ADDRESS);
+
+			if (verbose) {
+				System.out.println("Connected to device. OK.");
+			}
+
+			// Resetting
+			servoDriver.write(MODE1, (byte) 0x00);
+
+			// Set frequency
+			setPWMFreq(HERTZ);
+		} catch (IOException | UnsatisfiedLinkError e) {
+			System.err.println(e.getMessage());
 		}
 	}
-/*
-	private void setServoPulse(int channel, int pulse)
-	{
-		int pulseLength = 1000000; // 1,000,000 us per second
-		pulseLength /= HERTZ;
-		
-		if (verbose) {
-	      System.out.println(pulseLength + " us per period");
-		}
-		pulseLength /= 4096;       // 12 bits of resolution
-		if (verbose) {
-			System.out.println(pulseLength + " us per bit");
-		}
-		pulse *= 1000;
-		pulse /= pulseLength;
-		this.setPWM(channel, 0, pulse);
-	}
-*/
 }
