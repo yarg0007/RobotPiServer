8306b97d96307e2486d99f55e12305bfda22f657
Benjamin Yarger <benjamin.yarger@gmail.com>
2023-12-17 10:41:27 -0800

    Attempt to source OpenAPI document from url's spec query param.

--- swaggerdist/swagger-initializer.js
+++ swaggerdist/swagger-initializer.js
@@ -1,9 +1,16 @@
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
+  var queryStr = window.location.search;
+  var paramPairs = queryStr.substr(1).split('&');
+  var params = {};
+  for (var i = 0; i < paramPairs.length; i++) {
+      var parts = paramPairs[i].split('=');
+      params[parts[0]] = parts[1];
+  }
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://yarg0007.github.io/RobotPiServer/src/main/resources/api/openapi.yaml",
+    url: params.spec,
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
